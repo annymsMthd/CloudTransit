8d9d76da35778788bb845d136df1331525b466e0
Joshua Benjamin <joshuabnjmn@gmail.com>
2013-07-13 19:44:43 -0700

    Minor cleanup on gps functions

--- Gps/Gps.hs
+++ Gps/Gps.hs
@@ -4,18 +4,14 @@ import GpsTypes
 
 earthRadius = 6371
 
-toRadians degrees =
-    pi / 180 * degrees
+toRadians degrees = pi / 180 * degrees
 
-toDegrees radian =
-    180 / pi * radian
+toDegrees radian = 180 / pi * radian
 
-sin2 radian = 
-    sin (radian) ^ 2    
+sin2 radian = sin radian ^ 2    
 
 distance :: GpsPosition -> GpsPosition -> Double
-distance pos1 pos2 = 
-        earthRadius * c
+distance pos1 pos2 = earthRadius * c
     where dLat = lat2 - lat1
           dLon = toRadians $ longitude pos2 - longitude pos1
           lat1 = toRadians . latitude $ pos1
@@ -24,8 +20,7 @@ distance pos1 pos2 =
           c = 2 * atan2 (sqrt a) (sqrt $ 1-a)
 
 bearing :: GpsPosition -> GpsPosition -> Double
-bearing pos1 pos2 =
-    toDegrees $ atan2 y x
+bearing pos1 pos2 = toDegrees $ atan2 y x
     where dLon = toRadians $ longitude pos2 - longitude pos1
           lat1 = toRadians . latitude $ pos1
           lat2 = toRadians . latitude $ pos2
@@ -33,8 +28,7 @@ bearing pos1 pos2 =
           x = cos lat1 * sin lat2 - sin lat1 * cos lat2 * cos dLon
 
 positionDifference :: GpsPosition -> GpsPosition -> PositionDifference
-positionDifference gps1 gps2 =
-        PositionDifference dist heading timeDifference
+positionDifference gps1 gps2 = PositionDifference dist heading timeDifference
     where dist = distance gps1 gps2
           heading  = bearing gps1 gps2
           timeDifference = time gps2 - time gps1
