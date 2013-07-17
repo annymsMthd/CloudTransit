a3a45816dee943a59f3621c99c2eaa3437a98744
Joshua Benjamin <joshuabnjmn@gmail.com>
2013-07-16 20:39:17 -0700

    Fixed gps test

--- tests/GpsTest.hs
+++ tests/GpsTest.hs
@@ -9,7 +9,7 @@ import Test.QuickCheck
 import Test.HUnit
 import Control.Exception (evaluate)
 
-gpsSpecs = _ <- describe "Gps Tests" $ do
+gpsSpecs = describe "Gps Tests" $ do
                 let gps1 = GpsPosition 34.064828 (-118.361379) 0
                 let gps2 = GpsPosition 34.064899 (-118.351455) 20                    
                 let combined = compressPositions $ gps1:[gps2]
@@ -38,7 +38,7 @@ gpsUncmprss = describe "Compress Gps Points" $ do
                 let diff = [positionDifference (fst diff) (snd diff) | diff <- zip umcom path]
                 _ <- it "should have 2 gps positions" $ length umcom == 5
                 _ <- it "should be the same gps positions" $ all (<0.00000001) [distanceD d | d <- diff]
-                it "should be the same times" [time d | d <- path] == [time d | d <- umcom]
+                it "should be the same times" $ [time d | d <- path] `shouldBe` [time d | d <- umcom]
 
 --test3 = TestCase $ do
                    --  let gps1 = GpsPosition 34.064828 (-118.361379) 0
