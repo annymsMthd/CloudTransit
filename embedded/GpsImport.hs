bc2fdcc1474bc21a1135b44f2922c9685e639ab5
annymsMthd <joshuabnjmn@gmail.com>
2013-07-22 00:01:49 -0700

    Delete GpsImport.hs

--- embedded/GpsImport.hs
+++
@@ -1,7 +0,0 @@
-module GpsImport
-    ( module GpsImport) where
-
-import           Gps.Gps                   as   GpsImport
-import           Gps.GpsTypes              as   GpsImport
-import           Gps.MessageTypes          as   GpsImport
-import           Gps.MessageTypeConversion as   GpsImport
